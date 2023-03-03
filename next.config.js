a09311d0a2a40b0018631f40595e1edb34c3bdc2
Peter Brczán <peter.brczan.developer@gmail.com>
2023-03-03 13:26:34 +0100

    [master]: added workaround and updated the README file.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  // https://stackoverflow.com/questions/67333737/add-crypto-browserify-to-gatsby-project
+  webpack: config => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        ...config.resolve.fallback,
+        "crypto": require.resolve('crypto-browserify')
+      }
+    }
+
+    return config
+  }
 }
 
 module.exports = nextConfig
